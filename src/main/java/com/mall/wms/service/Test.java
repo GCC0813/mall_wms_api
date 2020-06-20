069def84e91f537e56143ede7e16529f000994e2
葛晨晨 <gechenchen@peilian.com>
2020-06-20 21:55:37 +0800

    fix

--- src/main/java/com/mall/wms/service/Test.java
+++ src/main/java/com/mall/wms/service/Test.java
@@ -15,11 +15,11 @@ public class Test {
 
     public static void main(String[] args) throws Exception {
 
-        Class clazz = Class.forName("com.mall.wms.entity.GoodsCategoryEntity");
+      /*  Class clazz = Class.forName("com.mall.wms.entity.GoodsCategoryEntity");
         Constructor constructor = clazz.getConstructor(null);
         GoodsCategoryEntity entity = (GoodsCategoryEntity)constructor.newInstance();
         entity.setStatus(1);
-        System.out.println(entity);
+        System.out.println(entity);*/
     }
 
 }
